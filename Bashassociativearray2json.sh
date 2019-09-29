cbfd8d1d9a8433307a9e90208c44e3a22551313c
Fontana <matthieu.fontana@itcloud.ovh>
2019-09-29 14:22:41 +0000

    Update Bashassociativearray2json.sh

--- Bashassociativearray2json.sh
+++ Bashassociativearray2json.sh
@@ -18,7 +18,7 @@ jq -n -R 'reduce inputs as $l ({}; . + { ($l): (input|(tostring? // .)) })'
 
 i=0
 while [ $i -lt `cat "$jsonfile" | jq '. | length'` ] ; do
-   tri=`cat "___error_log.json" | jq -r ".[$i].appli_error[]"`
+   tri=`cat "$jsonfile" | jq -r ".[$i].appli_error[]"`
    input=$(for k in ${tri[*]}; do cat "$hostnamefile" | grep $k ; done | sed 's/,//g')
    cat ___error_log.json | jq ".[$i]" | jq --argjson hstnm "`bash2array`" '. |= . + {servers: $hstnm}'
 i=$(($i + 1)); done
