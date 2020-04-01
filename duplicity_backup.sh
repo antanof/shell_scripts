68de5a730cf1b99cabad58d79112c1d7d46590ae
Fontana <matthieu.fontana@itcloud.ovh>
2020-04-01 23:39:16 +0000

    Create duplicity_backup.sh

---
+++ duplicity_backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+KEY="AF779YF8" # CHANGEME
+BACKUP_DOC="/"
+REMOTE_SERVER="xxx.xxx.xxx.xxx" # CHANGEME
+USER="matt" # CHANGEME
+DEST_DIR="remotedata/`hostname -f`"
+DEST="rsync://$USER@$REMOTE_SERVER/$DEST_DIR"
+
+apt update && apt install duplicity -y
+
+ssh $USER@$REMOTE_SERVER "mkdir -p $DEST_DIR"
+
+PASSPHRASE="blablablah" \
+duplicity --encrypt-key $KEY \
+          --exclude=/proc \
+          --exclude=/sys \
+          --exclude=/mnt \
+          --exclude=/tmp \
+          --exclude=/dev \
+          --exclude=/var/spool \
+          --exclude=/var/cache \
+          --exclude=/var/tmp \
+          $BACKUP_DOC \
+          $DEST
+exit 0
+#EOF
